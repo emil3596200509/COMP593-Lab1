c2d062b30d380b0c28d10c0fca2ff3087d7065b6
emil3596200509 <egeorge@flemingcollege.ca>
2024-05-29 16:00:31 -0400

    modified both files in repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statement to script2.py

--- script1.py
+++ script1.py
@@ -1,2 +1,1 @@
 # print lines from zen of python
-print("Special cases aren't special enough to break the rules.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #print lines from Zen of Pyhton
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("In the face of ambiguity, refuse the temptation to guess.")
+print("Namespaces are one honking great idea -- let's do more of those!")
